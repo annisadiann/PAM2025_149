b94ae2f245dff2e49028f8a54fd2e3fe17bc78d3
annisadiann <annisadian0412@gmail.com>
2026-01-16 21:26:30 +0700

    add file PemeriksaanRepository.kt

---
+++ app/src/main/java/com/example/posyandu/data/repository/PemeriksaanRepository.kt
@@ -0,0 +1,24 @@
+package com.example.posyandu.data.repository
+
+import com.example.posyandu.data.local.dao.PemeriksaanDao
+import com.example.posyandu.data.local.dao.StandarAntropometriDao
+import com.example.posyandu.data.local.entity.Pemeriksaan
+import kotlinx.coroutines.flow.Flow
+
+class PemeriksaanRepository(
+    private val pemeriksaanDao: PemeriksaanDao,
+    private val standarDao: StandarAntropometriDao
+) {
+    // Menyimpan hasil pemeriksaan
+    suspend fun insertPemeriksaan(pemeriksaan: Pemeriksaan) =
+        pemeriksaanDao.insertPemeriksaan(pemeriksaan)
+
+    // PERBAIKAN: Pastikan nama fungsi ini ada di PemeriksaanDao.kt
+    fun getRiwayat(idBalita: Int): Flow<List<Pemeriksaan>> =
+        pemeriksaanDao.getRiwayatPemeriksaan(idBalita)
+
+    // Logika perhitungan otomatis gizi
+    suspend fun hitungStatusGizi(umur: Int, jk: String, bb: Double, tb: Double): String {
+        return standarDao.getStatusGizi(umur, jk, bb, tb) ?: "Data Standar Tidak Ditemukan"
+    }
+}
