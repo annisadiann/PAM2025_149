666ed1859a5a875043640938960ca3c7f2cd6778
annisadiann <annisadian0412@gmail.com>
2026-01-16 21:32:29 +0700

    mengganti greeting manjadi func

---
+++ app/src/main/java/com/example/posyandu/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.posyandu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.posyandu.navigation.SetupNavGraph
+import com.example.posyandu.ui.theme.PosyanduTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Sesuai Activity Diagram: Entry point aplikasi dimulai dari sini
+        setContent {
+            // Menggunakan tema Pink yang telah kita buat sebelumnya
+            PosyanduTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Inisialisasi NavController untuk Jetpack Compose
+                    val navController = rememberNavController()
+
+                    // Memanggil NavGraph yang mengatur alur Splash -> Login -> Dashboard
+                    // Greeting logic sudah terintegrasi di dalam DashboardScreen
+                    SetupNavGraph(navController = navController)
+                }
+            }
+        }
+    }
+}
