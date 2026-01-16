29a86c0c88dbcb97360e37806591f341d127a7e8
annisadiann <annisadian0412@gmail.com>
2026-01-16 21:22:55 +0700

    add file LaporanDao.kt

---
+++ app/src/main/java/com/example/posyandu/data/local/dao/LaporanDao.kt
@@ -0,0 +1,18 @@
+package com.example.posyandu.data.local.dao
+
+import androidx.room.*
+import com.example.posyandu.data.local.entity.Laporan
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LaporanDao {
+    @Insert
+    suspend fun simpanLaporan(laporan: Laporan)
+
+    // REQ-LAP-4: Ambil data sesuai periode [cite: 251]
+    @Query("SELECT * FROM laporan WHERE periode BETWEEN :start AND :end")
+    suspend fun getLaporanByPeriode(start: Long, end: Long): List<Laporan>
+
+    @Query("SELECT * FROM laporan ORDER BY created_at DESC")
+    fun getAllLaporan(): Flow<List<Laporan>>
+}
